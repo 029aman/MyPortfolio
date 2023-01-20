17b46006f70a4a1125ab6326925b0b8daae6504f
029aman <029amankumar@gamil.com>
2023-01-20 19:12:38 +0530

    Added Live Codeforces Rating

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
         $('html').css("scrollBehavior", "smooth");
     });
 
 
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
@@ -44,18 +44,40 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
         }
     });
 });
+
+
+// Codeforces Live Rating
+const cfRatingHigh = document.querySelector('#cfRatingHigh');
+const cfRatingCurr = document.querySelector('#cfRatingCurr');
+
+async function getCFRating() {
+    const res = await fetch("https://codeforces.com/api/user.rating?handle=029aman")
+    const data = await res.json();
+    return data;
+}
+
+async function callCF() {
+    var rating = await getCFRating();
+    var maxR=0;
+    for(let a of rating.result){
+        maxR= Math.max(maxR, a.newRating);
+    }
+    cfRatingCurr.innerHTML = rating.result[rating.result.length-1].newRating;
+    cfRatingHigh.innerHTML = maxR; 
+}
+callCF();
